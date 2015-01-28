acad98dfcb569bac8f512db4c0e68e66158d0ded
Andrew Chen <deweichen47@gmail.com>
2015-01-28 16:02:07 -0500

    next line shift working

--- space.py
+++ space.py
@@ -1,5 +1,12 @@
 import sys
 
+class Vec(object):
+
+    def __init__(self, x, y):
+        self.x = int(x) 
+        self.y = int(y)
+
+
 class Space(object):
 
     def __init__(self, frame_width, frame_height, lim_x, lim_y):
@@ -12,7 +19,15 @@ class Space(object):
         self._mat = [[None] * self._lim_x for i in range(self._lim_y)]
 
 
-    def _shift_x(self, s):
+    def range_x(self, x):
+        return x >= 0 and x < self._lim_x
+
+
+    def range_y(self, y):
+        return y >= 0 and y < self._lim_y
+
+
+    def shift_x(self, s):
         if s > 0 and self._x + self._wd + s > self._lim_x:
             self._x = self._lim_x - self._wd
         elif s < 0 and self._x + s < 0:
@@ -21,7 +36,7 @@ class Space(object):
             self._x += s
 
 
-    def _shift_y(self, s):
+    def shift_y(self, s):
         if s > 0 and self._y + self._ht + s > self._lim_y:
             self._y = self._lim_y - self._ht
         elif s < 0 and self._y + s < 0:
@@ -30,7 +45,7 @@ class Space(object):
             self._y += s
 
 
-    def _putc(self, x, y, c):
+    def putc(self, x, y, c):
         if type(c) is not str or len(c) > 1:
             return
         if x > self._lim_x:
@@ -44,7 +59,7 @@ class Space(object):
         self._mat[y][x] = str(c)
 
 
-    def _draw_frame(self): 
+    def draw_frame(self): 
         sys.stdout.write("".join(["-"] * (self._wd + 2 )) + "\n")
         for row in self._mat[self._y:(self._y+self._ht)]: 
             line = map(lambda x : " " if x == None else x, row[self._x:(self._x+self._wd)])
--- tcan.py
+++ tcan.py
@@ -1,50 +1,42 @@
 #!/usr/bin/python2
 
 import sys
-import space
+from space import Space, Vec
 
 class Can(object):
 
     def __init__(self, frame_width, frame_height, lim_x=1000, lim_y=1000):
-        self.space = space.Space(frame_width, frame_height, lim_x, lim_y)
+        self._space = Space(frame_width, frame_height, lim_x, lim_y)
 
 
-    def print_int(self, coord, int_):
-        if type(int_) is not int:
-            return 
-        for c in str(int_):
-            self.space._putc(coord[0], coord[1], c)
-            x += 1
-
-
-    def print_str(self, coord, str_):
-        if type(str_) is not str:
+    def printv(self, val, vec):
+        if type(vec) is not Vec:
             return
-        for c in str_:
-            self.space._putc(coord[0], coord[1], c) 
-            x += 1
-
+        for c in str(val):
+            self._space.putc(vec.x, vec.y, c) 
+            vec.x += 1
+            if not self._space.range_x(vec.x):
+                vec.x = 0
+                vec.y += 1
     
-    def move_frame_x(self, s):
-        if type(s) is not int:
+
+    def move_frame(self, vec):
+        if type(vec) is not Vec:
             return
-        self.space._shift_x(s)
-
-
-    def move_frame_y(self, s):
-        self.space._shift_y(s)
+        self._space.shift_x(vec.x)
+        self._space.shift_y(vec.y)
 
 
     def draw(self):
-        self.space._draw_frame()
+        self._space.draw_frame()
         
 
 if __name__ == "__main__":
     
-    canvas = Can(80, 30, 80, 30)
-    canvas.print_int((0, 0), 1234)
-    canvas.print_str((4, 4), "hello")
-    canvas.move_frame_x(2)
-    canvas.move_frame_y(3)
+    canvas = Can(80, 30, 800, 300)
+    canvas.printv(1234, Vec(0, 0))
+    canvas.printv("hello world my name is Dewei", Vec(75, 4))
+    canvas.printv(5.52e10, Vec(6, 10))
+    canvas.move_frame(Vec(10, 0))
     canvas.draw()
 
