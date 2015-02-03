0aa5484a8da20fead578915e689aab2073732fe3
Andrew Chen <deweichen47@gmail.com>
2015-02-02 23:49:25 -0500

    changed name

--- textframe.py
+++ textframe.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python2
 
 import sys
+import os
+from threading import Thread
+
 from space import Space, Vec
 
-class Can(object):
+class TextFrame(object):
 
     def __init__(self, frame_width, frame_height, lim_x=1000, lim_y=1000,
                  wrap_x=True, wrap_y=False, inc_x=1, inc_y=1):
@@ -17,7 +20,7 @@ class Can(object):
 
 
     def printh(self, val, vec):
-        """Print val horizontally"""
+        """Print horizontally"""
         if type(vec) is not Vec:
             return
         for c in str(val):
@@ -31,7 +34,7 @@ class Can(object):
 
 
     def printv(self, val, vec):
-        """Print val vertically"""
+        """Print vertically"""
         if type(vec) is not Vec:
             return
         for c in str(val):
@@ -42,25 +45,28 @@ class Can(object):
                 vec.x = 0
             self._space.putc(vec.x, vec.y, c)
             vec.y += self._inc_y
-            
 
-    def move_frame(self, vec):
+
+    def move(self, vec):
         if type(vec) is not Vec:
             return
         self._space.shift_x(vec.x)
         self._space.shift_y(vec.y)
 
     # TODO: ruler
+    # 0----+----1----+----2
     def draw(self, ruler=False):
         self._space.draw_frame()
         
 
 if __name__ == "__main__":
     
-    canvas = Can(80, 30, 80, 30, inc_x=2)
-    canvas.printh(1234, Vec(0, 0))
-    canvas.printh("hello world my name is Dewei", Vec(75, 4))
-    canvas.printh(5.52e10, Vec(6, 10))
-    canvas.move_frame(Vec(10, 0))
-    canvas.draw()
+    frame = TextFrame(80, 30, lim_x=80, lim_y=30)
+    frame.printh(1234, Vec(0, 0))
+    frame.printh("hello world my name is Dewei", Vec(75, 4))
+    frame.printh(5.52e10, Vec(6, 10))
+    frame.move(Vec(10, 0))
+    while True:
+        frame.draw()
+        os.system('cls' if os.name == 'nt' else 'clear')    
 
